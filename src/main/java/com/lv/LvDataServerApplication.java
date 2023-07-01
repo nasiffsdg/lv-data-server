0ef851833c948801d3a21b9abf1453466d1a6580
lv <77777777@qq.com>
2023-07-01 23:31:24 +0800

    init

---
+++ src/main/java/com/lv/LvDataServerApplication.java
@@ -0,0 +1,13 @@
+package com.lv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LvDataServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LvDataServerApplication.class, args);
+	}
+
+}
