e1daf9f9afc19d2a9bb332a80377302a6360d0e9
lv <77777777@qq.com>
2023-07-05 23:53:56 +0800

    data

--- src/main/java/com/lv/LvDataServerApplication.java
+++ src/main/java/com/lv/LvDataServerApplication.java
@@ -1,8 +1,14 @@
 package com.lv;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author lvxueqiang
+ */
 @SpringBootApplication
 public class LvDataServerApplication {
 
